8ddc79863a59a8f5b38c3af0895522cb633dc288
zoner16 <bradpowell730@gmail.com>
2016-09-15 17:10:03 -0400

    Updated Lab1

--- Lab1/lab1.sv
+++ Lab1/lab1.sv
@@ -21,10 +21,10 @@
 
 
 module lab1(
-    input clk, sw[0], btnC, btnU,
-    output Hsync, Vsync,
-    output [3:0] vgaRed, vgaBlue, vgaGreen,
-    output [7:0] led  
+    input sys_clk_pin, SW[1:0], BTNC, BTNU,
+    output VGA_HS, VGA_VS,
+    output [3:0] VGA_R, VGA_B, VGA_G,
+    output [7:0] LED  
     );
     
     logic hs, vs; 
@@ -32,28 +32,24 @@ module lab1(
     logic [9:0] h_count;
     logic [23:0] v_count;
     
-    enum {V_PULSE, V_SYNC} V_STATE;
-    enum {H_PULSE, H_SYNC} H_STATE; 
+    enum {V_PULSE, V_FRONT, V_DISP, V_BACK} V_STATE;
+    enum {H_PULSE, H_FRONT, H_DISP, H_BACK} H_STATE; 
     enum {UNPRESSED, PRESSED} B_STATE;
     
-    assign led[3:0] = count;
-    assign led[7:4] = 4'b1111; 
-    assign Hsync = hs;
-    assign Vsync = vs;
-    assign vgaRed = count[3:0];
-    assign vgaBlue = count[3:0];
-    assign vgaGreen = count[3:0];
+    assign LED[3:0] = count;
+    assign LED[7:4] = 4'b1111; 
+    assign VGA_HS = H_STATE != H_PULSE;
+    assign VGA_VS = V_STATE != V_PULSE;
+    assign VGA_R = (H_STATE == H_DISP) ? count : 0;
+    assign VGA_B = (H_STATE == H_DISP) ? count : 0;
+    assign VGA_G = (H_STATE == H_DISP) ? count : 0;
     
     
-    always_ff @(posedge clk, negedge btnU) begin
-        if(~btnU) begin
+    always_ff @(posedge sys_clk_pin, posedge BTNU) begin
+        if(BTNU) begin
             count = 0;
             v_count = 0;
             h_count = 0;
-            hs = 0;
-            vs = 0;
-            V_STATE = V_PULSE;
-            H_STATE = H_PULSE;
             B_STATE = UNPRESSED;
         end
         else begin
@@ -64,55 +60,18 @@ module lab1(
                 v_count <= 0;
             end
             
-            case(V_STATE)
-                V_PULSE: begin
-                    if(v_count < 1699) begin
-                        hs <= 0;
-                        vs <= 0; 
-                        h_count <= 0;           
-                    end
-                    else begin
-                        V_STATE <= V_SYNC;
-                    end
-                end
-                V_SYNC: begin
-                    if(h_count < 799) begin
-                        h_count <= h_count + 1;
-                    end
-                    else begin
-                        h_count <= 0;
-                    end
-                    
-                    case(H_STATE) 
-                        H_PULSE: begin
-                            hs <= 0;
-                        end
-                        H_STATE: begin
-                            if(h_count < 1599) begin
-                                hs <= 1;
-                            end
-                            else begin
-                                hs <= 0;
-                                H_STATE <= H_PULSE;
-                            end
-                        end
-                    endcase
-                
-                    if(v_count < 833599) begin
-                        vs <= 1;
-                    end
-                    else begin
-                        vs <= 0;
-                        V_STATE <= V_PULSE;
-                    end
-                end
-            endcase
+            if(h_count < 799 && V_STATE == V_DISP) begin
+                h_count <= h_count + 1;
+            end
+            else begin
+                h_count <= 0;
+            end
         
             case(B_STATE) 
                 UNPRESSED: begin
-                    if(btnC) begin
+                    if(BTNC) begin
                         B_STATE <= PRESSED;
-                        if(sw[0]) begin
+                        if(SW[0]) begin
                             count <= count + 1;
                         end
                         else begin
@@ -121,12 +80,40 @@ module lab1(
                     end
                 end
                 PRESSED: begin
-                    if(~btnC) begin
+                    if(~BTNC) begin
                         B_STATE <= UNPRESSED;
                     end
                 end
             endcase
         end
+    end
+    
+    always_comb begin
+        if(v_count < 7999) begin
+            V_STATE = V_FRONT;
+        end
+        else if(v_count < 9599) begin
+            V_STATE = V_PULSE;
+        end
+        else if(v_count < 32799) begin
+            V_STATE = V_BACK;
+        end
+        else begin
+            V_STATE = V_DISP;
+        end 
+        
+        if(v_count < 15) begin
+            H_STATE = H_FRONT;
+        end
+        else if(h_count < 111) begin
+            H_STATE = H_PULSE;
+        end
+        else if(h_count < 159) begin
+            H_STATE = H_BACK;
+        end
+        else begin
+            H_STATE = H_DISP;
+        end 
     end
     
 endmodule
