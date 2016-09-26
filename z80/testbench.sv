f7ab5c31c306ef1b79bc7e17115adaa8ce3d7e2d
Jason Kim <jason.gwkim@gmail.com>
2016-09-26 07:57:19 -0400

    Project files for z80

--- z80/testbench.sv
+++ z80/testbench.sv
@@ -2,23 +2,23 @@ module test_z80();
 `include "z80_top_direct_n.v"
 
 wire nM1,
-wire nMREQ,
-wire nIORQ,
-wire nRD,
-wire nWR,
-wire nRFSH,
-wire nHALT,
-wire nBUSACK,
-
-wire nWAIT,
-wire nINT,
-wire nNMI,
-wire nRESET,
-wire nBUSRQ,
-
-wire CLK,
-wire [15:0] A,
-wire [7:0] D
+     nMREQ,
+     nIORQ,
+     nRD,
+     nWR,
+     nRFSH,
+     nHALT,
+     nBUSACK,
+     
+     nWAIT,
+     nINT,
+     nNMI,
+     nRESET,
+     nBUSRQ,
+     
+     CLK;
+wire [15:0] A;
+wire [7:0] D;
 
 initial begin
     CLK = 0;
@@ -45,5 +45,6 @@ z80_top_direct_n z80(
     .nBUSRQ,
     .CLK,
     .A,
-    .D)
-
+    .D);
+    
+endmodule
