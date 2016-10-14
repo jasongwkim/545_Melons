55a7744593a916648e2e788679a4dafba55f5700
Jason Kim <jason.gwkim@gmail.com>
2016-10-14 13:37:34 -0400

    Setting up initial ports for Z80

--- Midterm_Review/Review_Top.sv
+++ Midterm_Review/Review_Top.sv
@@ -12,27 +12,16 @@ module demo (
     logic [15:0] Z80_addr;
     logic [7:0]  Z80_data;
     
+    logic nM1, nMREQ, nIORQ, nRD, nWR, nRFSH, nHALT, nBUSACK;
+    logic nWAIT, nINT, nNMI, nBUSRQ;
+    
     TG68 core (.clk(clk), .reset(rst_n), .clkena_in(1'b1), .data_in(M68_data_in),
                 .IPL(3'b111), .dtack(dtack), .addr(M68_addr), .data_out(M68_data_out),
                 .as(as), .uds(uds), .lds(lds), .rw(rw), .enaRDreg(1'b1), .enaWRreg(1'b1),
                 .intack(intack));
                 
-    z80_top_direct_n core2 (.CLK(clk), .nRESET(rst_n), .A(Z80_addr), .D(Z80_data)
+    z80_top_direct_n core2 (.CLK(clk), .nRESET(rst_n), .A(Z80_addr), .D(Z80_data),
+                            .nM1, .nMREQ, .nIORQ, .nRD, .nWR, .nRFSH, .nHALT, .nBUSACK,
+                            .nWAIT, .nINT, .nNMI, .nBUSRQ);
                 
 endmodule
-
-module z80_top_direct_n(
-    output wire nM1,
-    output wire nMREQ,
-    output wire nIORQ,
-    output wire nRD,
-    output wire nWR,
-    output wire nRFSH,
-    output wire nHALT,
-    output wire nBUSACK,
-
-    input wire nWAIT,
-    input wire nINT,
-    input wire nNMI,
-    input wire nBUSRQ,
-);
